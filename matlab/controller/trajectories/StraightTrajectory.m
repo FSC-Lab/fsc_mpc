436e48eeffde8ca7c6fd224c30e3066ae6078a23
Hs293Go <hellston20a@gmail.com>
2023-03-13 01:32:40 -0400

    Fix straight trajectory

--- matlab/controller/trajectories/StraightTrajectory.m
+++ matlab/controller/trajectories/StraightTrajectory.m
@@ -7,13 +7,13 @@ function trajectory = StraightTrajectory(quad, traj_begin, traj_end, z, discreti
 
     % Calculate simulation time to achieve desired maximum velocity with specified
     % acceleration
-    t_total = distance / v_max;
+    t_cruise = distance / v_max - ramp_up_t; 
+%     t_total = t_cruise + 2.0 * ramp_up_t;
 
     refs = struct();
     a = struct();
     refs.ramp_up = 0:discretization_dt:(ramp_up_t - discretization_dt);
     a.ramp_up = lin_acc * ones(size(refs.ramp_up));
-    t_cruise = t_total - 2 * ramp_up_t;
     refs.cruise = refs.ramp_up(end) + (0:discretization_dt:t_cruise);
 
     a.cruise = zeros(size(refs.cruise));
@@ -28,12 +28,13 @@ function trajectory = StraightTrajectory(quad, traj_begin, traj_end, z, discreti
     n = length(t_ref);
     traj = zeros(4, 3, n);
     traj(1, 1:2, :) = traj_begin + heading_vector .* d_vec;
+    assert(norm(traj(1, 1:2, end) - reshape(traj_end, 1, [])) < 1);
     traj(1, 3, :) = z;
     traj(2, 1:2, :) = heading_vector * v_vec;
     traj(3, 1:2, :) = heading_vector * a_vec;
     yaw = zeros(2, n);
     yaw(1, :) = atan2(position_diff(2), position_diff(1));
 
-    [traj_ref, u_ref, t_ref] = MinimumSnapTrajectoryGenerator(traj, yaw, t_ref, quad);
+    [traj_ref, u_ref, t_ref] = MinimumSnapTrajectoryGenerator(traj, yaw, t_ref, 'QuadMass', quad.mass, 'StartAtZero', false, 'UseBodyFrameDynamics', true);
     trajectory = struct('states', traj_ref, 'inputs', u_ref, 'time', t_ref);
 end
